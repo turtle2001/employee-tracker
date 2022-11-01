b0d4f3a24fa98a901189d67cda1e4e96b99ce5cd
Alexis Zaragoza Jr <alexis.oakland21@gmail.com>
2022-11-01 04:16:12 -0700

    bugfixes

--- schema.sql
+++ schema.sql
@@ -5,25 +5,26 @@ CREATE DATABASE buisness_db;
 USE buisness_db;
 
 CREATE TABLE department (
-    id INT,
+    id INT NOT NULL,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT,
+    id INT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
     department_id INT,
+    PRIMARY KEY(id),
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE employee(
-    id INT,
+CREATE TABLE employee (
+    id INT NOT NULL,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id)
-)
+    FOREIGN KEY (manager_id) REFERENCES role(id)
+);
--- seeds.sql
+++ seeds.sql
@@ -1,12 +1,11 @@
-INSERT INTO
-    department (id, name)
+INSERT INTO department (id, name)
 VALUES
     (1, "Sales"),
     (2, "Engineering"),
     (3, "Finance"),
-    (4, "Legal"),
-INSERT INTO
-    role (id, title, salary, department_id)
+    (4, "Legal");
+
+INSERT INTO role (id, title, salary, department_id)
 VALUES
     (1, "Sales Lead", 100000, 1),
     (2, "Salesperson", 80000, 1),
@@ -15,15 +14,15 @@ VALUES
     (5, "Account Manager", 160000, 3),
     (6, "Accountant", 125000, 3),
     (7, "Legal Team Lead", 250000, 4),
-    (8, "Lawyer", 190000, 4)
-INSERT INTO
-    employee(id, first_name, last_name, role_id, manager_id)
+    (8, "Lawyer", 190000, 4);
+
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
 VALUES
-    (1, "John", "Doe", 1, 0),
+    (1, "John", "Doe", 1, null),
     (2, "Mike", "Chan", 2, 1),
-    (3, "Ashley", "Rodriguez", 3, 0),
+    (3, "Ashley", "Rodriguez", 3, null),
     (4, "Kevin", "Tupik", 4, 3),
-    (5, "Kunal", "Singh", 5, 0),
+    (5, "Kunal", "Singh", 5, null),
     (6, "Malia", "Brown", 6, 5),
-    (7, "Sarah", "Lourd", 7, 0),
-    (8, "Tom", "Allen", 8, 7),
+    (7, "Sarah", "Lourd", 7, null),
+    (8, "Tom", "Allen", 8, 7);

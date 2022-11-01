c5cd8a1a3f9c1a5ea094c51a76cfdea51392e841
Alexis Zaragoza Jr <alexis.oakland21@gmail.com>
2022-11-01 05:39:43 -0700

    inital prompts

--- server.js
+++ server.js
@@ -1,11 +1,60 @@
-const express = require('express');
-// Import and require mysql2
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const api = require('./routes/index')
+const cTable = require('console.table');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // MySQL password
+        password: 'Oaklandaz921.',
+        database: 'movies_db'
+    },
+    console.log(`Connected to the movies_db database.`)
+);
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+function start() {
+    inquirer
+        .prompt(
+            {
+                type: 'list',
+                message: 'What would you like to do?',
+                name: 'choice',
+                choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role',
+                    'View All Departments', 'Add Department', 'Quit', 'View All Employees']
+            }
+        )
+        .then((res) => {
+            switch (res.choice) {
+                case 'View All Employees':
+                    viewEmployees();
+                    break;
+                case 'Add Employee':
+                    addIntern();
+                    break;
+                case 'Update Employee Role':
+                    generateHTML(employees)
+                    break;
+                case 'View All Roles':
+                    addEngineer();
+                    break;
+                case 'Add Role':
+                    addIntern();
+                    break;
+                case 'View All Departments':
+                    generateHTML(employees)
+                    break;
+                case 'Add Department':
+                    addEngineer();
+                    break;
+                case 'Quit':
+                    addIntern();
+                    break;
+                case 'View All Employees':
+                    generateHTML(employees)
+                    break;
+            }
+        })
+}
+
